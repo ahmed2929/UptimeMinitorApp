36abc2b8ee601a7176b30188c809a18cd4dcd1de
ahmed2929 <mywork304@gmail.com>
2022-12-15 21:50:16 +0200

    ignore env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let app =require("express")();
 require('dotenv').config();
 const  {ConnetToDB} =require("./DB/Server/index")
 const SetMiddleWares=require('./Middlewares/index')
-const port = process.env.Port || 4000
+const port = process.env.PORT || 4000
 
 //Connect to DB
 ConnetToDB();
